0398f72bcd0c212bd2387d13ab1493cd268d7b31
GIRAUDEV <giraudev@gmail.com>
2022-12-05 16:12:06 +0000

    conversion

--- types/conversion/begin/main.go
+++ types/conversion/begin/main.go
@@ -1,6 +1,13 @@
 // types/conversion/begin/main.go
 package main
 
+import "fmt"
+
 func main() {
 	// declare float and convert it to an unsigned int
+	a:= 42.5
+	b:= uint(a)
+	fmt.Println("%v=%T, %v=%T", a, a, b, b)
+	fmt.Printf("%v=%T, %v=%T", a, a, b, b)
+
 }
